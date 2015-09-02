929a1a846f801eec118a7b83f5c480f256727ecc
ed1621 <eduardo.giovanni1621@gmail.com>
2015-09-01 20:48:19 -0600

    reduced total streams requested from get request

--- app/scripts/services/services-dota.js
+++ app/scripts/services/services-dota.js
@@ -4,7 +4,7 @@ angular.module('mobaTwitchApp')
 
 .service('dota', function($http){
   this.getGame = function(){
-    var url = 'https://api.twitch.tv/kraken/streams?game=Dota+2&limit=100&offset=0&callback=JSON_CALLBACK';
+    var url = 'https://api.twitch.tv/kraken/streams?game=Dota+2&limit=30&offset=0&callback=JSON_CALLBACK';
     return  $http.jsonp(url);
   };
 
--- app/scripts/services/services-lol.js
+++ app/scripts/services/services-lol.js
@@ -4,7 +4,7 @@ angular.module('mobaTwitchApp')
 
 .service('lol', function($http){
   this.getGame = function(){
-    var url = 'https://api.twitch.tv/kraken/streams?game=League+of+Legends&limit=100&offset=0&callback=JSON_CALLBACK';
+    var url = 'https://api.twitch.tv/kraken/streams?game=League+of+Legends&limit=30&offset=0&callback=JSON_CALLBACK';
     return  $http.jsonp(url);
   };
 
